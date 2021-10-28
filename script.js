ef2876107df668c1f6cf6887431d6230dca37996
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-10-28 11:09:35 -0300

    I created the accordion list function from the section .faq-lista

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-const tabMenu = document.querySelectorAll(".js-tabMenu li");
-const tabContent = document.querySelectorAll(".js-tabContent section");
+function initTabNavigation() {
+  const tabMenu = document.querySelectorAll(".js-tabMenu li");
+  const tabContent = document.querySelectorAll(".js-tabContent section");
 
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add("ativo");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
 
-  const activeTab = (index) => {
-    tabContent.forEach((section) => {
-      section.classList.remove("ativo");
+    const activeTab = (index) => {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    };
+
+    tabMenu.forEach((item, index) => {
+      item.addEventListener("click", () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add("ativo");
-  };
-
-  tabMenu.forEach((item, index) => {
-    item.addEventListener("click", () => {
-      activeTab(index);
-    });
-  });
+  }
 }
+initTabNavigation();
+
+function initAccordionList() {
+  const activeClass = "ativo"
+  const accordionItem = document.querySelectorAll(".js-accordion dt");
+
+  if (accordionItem.length) {
+    accordionItem[0].classList.add(activeClass);
+    accordionItem[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionItem.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+initAccordionList();
