5b76602be556c049758131ab9e11bed55e8c003f
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-10-29 08:52:12 -0300

    :sparkles: I created the animated scroll function

--- script.js
+++ script.js
@@ -66,3 +66,23 @@ function initScrollSuave() {
     link.addEventListener("click", scrollToSection);
   });
 }
+
+function initScrollAnimation() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowHalf = window.innerHeight * 0.6;
+
+    const animateScroll = () => {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowHalf < 0;
+
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
+    };
+    animateScroll();
+    window.addEventListener("scroll", animateScroll);
+  }
+}
+initScrollAnimation()
