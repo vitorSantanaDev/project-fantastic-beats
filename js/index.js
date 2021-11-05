f8810d6a5b8830e161a4dca3c652fe0647fb3f20
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-05 11:51:43 -0300

    ✨ Tooltip created! Styling and finished configurations

--- js/index.js
+++ js/index.js
@@ -3,9 +3,11 @@ import initScrollAnimation from "./modules/scrollAnimation.js";
 import initScrollSuave from "./modules/scrollSuave.js";
 import initAccordionList from "./modules/accordionList.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initScrollSuave();
 initScrollAnimation();
 initTabNavigation();
 initAccordionList();
 initModal();
+initTooltip();
