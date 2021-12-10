22a8f2f957c18cacbe9baaffd8addcf9da4c35b0
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-12-10 07:43:19 -0300

    :recycle: Class refactoring of animation to scroll

--- js/modules/scrollAnimation.js
+++ js/modules/scrollAnimation.js
@@ -1,20 +1,24 @@
-export default function initScrollAnimation() {
-  const sections = document.querySelectorAll('[data-anima="scroll"]');
-  if (sections.length) {
-    const windowHalf = window.innerHeight * 0.6;
+export default class ScrollAnimation {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowHalf = window.innerHeight * 0.6;
+    this.animateScroll = this.animateScroll.bind(this);
+  }
 
-    const animateScroll = () => {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = sectionTop - windowHalf < 0;
+  animateScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - this.windowHalf < 0;
 
-        if (isSectionVisible) section.classList.add("ativo");
-        else if(section.classList.contains("ativo")) {
-          section.classList.remove("ativo")
-        };
-      });
-    };
-    animateScroll();
-    window.addEventListener("scroll", animateScroll);
+      if (isSectionVisible) section.classList.add("ativo");
+      else if (section.classList.contains("ativo")) {
+        section.classList.remove("ativo");
+      }
+    });
+  }
+
+  init() {
+    this.animateScroll();
+    window.addEventListener("scroll", this.animateScroll);
   }
 }
